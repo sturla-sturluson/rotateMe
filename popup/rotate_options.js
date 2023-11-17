5645590373e1d6cfdef175d9173845ce1be70b4c
sturla-sturluson <sesrvkis@gmail.com>
2023-11-17 20:42:30 +0000

    storage working

--- popup/rotate_options.js
+++ popup/rotate_options.js
@@ -1,19 +1,23 @@
 const clockwiseCheckButton = document.querySelector("#rotationDirection");
 const savedPrompt = document.querySelector("#savedPrompt");
+const loadedP = document.querySelector("#loadedP");
 function saveOptions(e) {
-    savedPrompt.innerHTML = "Saved!";
     e.preventDefault();
+    savedPrompt.innerHTML = "Saving...";
     browser.storage.sync.set({
-        rotationDirection: document.querySelector("#rotationDirection").value,
+        rotationDirection: clockwiseCheckButton.checked,
     });
+    loadedP.innerHTML = "Saved!";
 }
 
 function restoreOptions() {
     function setCurrentChoice(result) {
+        loadedP.innerHTML = "Loaded result was: " + result.rotationDirection;
         clockwiseCheckButton.checked = result.rotationDirection || false;
     }
 
     function onError(error) {
+        loadedP.innerHTML = "Error loading " + error;
         console.log(`Error: ${error}`);
     }
 
