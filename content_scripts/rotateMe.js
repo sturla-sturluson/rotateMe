33d0a8bc00492bd67ca642b806e5855907fb3557
sturla-sturluson <sesrvkis@gmail.com>
2023-11-24 13:46:03 +0000

    updating

--- content_scripts/rotateMe.js
+++ content_scripts/rotateMe.js
@@ -1,19 +1,9 @@
 let shiftDown = false;
+let rotateClockWise = true;
+let savedSetting = true;
+let isEnabled = false;
 const degreeRegex = new RegExp('rotate\\((\\d+)deg\\)');
 
-const onError = (error) => {
-    console.log(`Error: ${error}`);
-}
-
-const onGot = (result) => {
-    const rotationDirection = result.rotationDirection;
-    if (rotationDirection === 'clockwise') {
-        rotatePage();
-    }
-}
-
-const getting = browser.storage.sync.get("rotationDirection");
-getting.then(onGot, onError);
 
 const findLargestImage = () => {
     const imgs = document.getElementsByTagName('img');
@@ -37,14 +27,29 @@ const getRotatedNumber = (rotateNumber) => {
     } else {
         rotateNumber = 0;
     }
-    rotateNumber += 90;
-    if (rotateNumber === 360) {
-        rotateNumber = 0;
-    }
-    return rotateNumber;
+    if (!rotateClockWise) {
+        return getCounterClockwiseRotatedNumber(rotateNumber);
+    } else
+        return getClockwiseRotatedNumber(rotateNumber);
 };
 
-const rotatePage = () => {
+const getClockwiseRotatedNumber = (rotateNumber) => {
+    rotateNumber += 90;
+    if (rotateNumber === 360) rotateNumber = 0;
+    return rotateNumber;
+};
+const getCounterClockwiseRotatedNumber = (rotateNumber) => {
+    rotateNumber -= 90;
+    if (rotateNumber === -90) rotateNumber = 270;
+    return rotateNumber;
+}
+
+const rotatePage = async () => {
+    if (!isEnabled) {
+        console.log("Plugin is disabled");
+        return
+    };
+    setClockwiseSetting();
     const img = findLargestImage();
     if (!img) return;
     let currentRotation = img.style.transform;
@@ -69,8 +74,45 @@ const settingImgStyle = (img, rotation) => {
     img.style = `transform: rotate(${rotation}deg); margin-top: ${difference / 2}px;`;
 };
 
+const setDisabledSetting = () => {
+    function setCurrentChoice(result) {
+        isEnabled = result.isEnabled;
+        if (isEnabled === undefined) {
+            isEnabled = false;
+            console.log("Setting is undefined, setting to false");
+        }
 
+    }
 
+    function onError(error) {
+        console.log(`Error: ${error}`);
+        isEnabled = false;
+    }
+    let getting = browser.storage.sync.get("isEnabled");
+    getting.then(setCurrentChoice, onError);
+    isEnabled = savedSetting;
+
+}
+
+const setClockwiseSetting = () => {
+    function setCurrentChoice(result) {
+        savedSetting = result.rotationDirection;
+        if (savedSetting === undefined) {
+            savedSetting = true;
+            console.log("Setting is undefined, setting to true");
+        }
+
+    }
+
+    function onError(error) {
+        console.log(`Error: ${error}`);
+        savedSetting = true;
+    }
+    let getting = browser.storage.sync.get("rotationDirection");
+    getting.then(setCurrentChoice, onError);
+    rotateClockWise = savedSetting;
+
+}
 
 
 document.addEventListener('keydown', (e) => {
@@ -91,3 +133,8 @@ document.addEventListener('keydown', (e) => {
         rotatePage();
     }
 })
+
+setDisabledSetting();
+setClockwiseSetting();
+
+
--- popup/rotate_options.js
+++ popup/rotate_options.js
@@ -1,18 +1,20 @@
 const clockwiseCheckButton = document.querySelector("#rotationDirection");
 const savedPrompt = document.querySelector("#savedPrompt");
 const loadedP = document.querySelector("#loadedP");
+const disableButton = document.querySelector("#disableButton");
+let isEnabled = false;
+
 function saveOptions(e) {
     e.preventDefault();
-    savedPrompt.innerHTML = "Saving...";
     browser.storage.sync.set({
         rotationDirection: clockwiseCheckButton.checked,
+        isEnabled: isEnabled
     });
     loadedP.innerHTML = "Saved!";
 }
 
-function restoreOptions() {
+function syncClockwiseSettings() {
     function setCurrentChoice(result) {
-        loadedP.innerHTML = "Loaded result was: " + result.rotationDirection;
         clockwiseCheckButton.checked = result.rotationDirection || false;
     }
 
@@ -25,6 +27,48 @@ function restoreOptions() {
     getting.then(setCurrentChoice, onError);
 }
 
+function syncDisableSettings() {
+    function setCurrentChoice(result) {
+        isEnabled = result.isEnabled;
+        if (isEnabled === undefined) {
+            isEnabled = false;
+            console.log("Setting is undefined, setting to false");
+        }
+    }
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
+    function onError(error) {
+        loadedP.innerHTML = "Error loading " + error;
+        console.log(`Error: ${error}`);
+    }
+    let getting = browser.storage.sync.get("isEnabled");
+    getting.then(setCurrentChoice, onError);
+    initialSetup();
+}
+
+const disableButtonAction = () => {
+    if (!isEnabled) {
+        disableButton.innerHTML = "Enable";
+        isEnabled = true;
+    }
+    else {
+        disableButton.innerHTML = "Disable";
+        isEnabled = false;
+    }
+    saveOptions();
+
+}
+
+const initialSetup = () => {
+    if (isEnabled) {
+        disableButton.innerHTML = "Disable";
+    }
+    else {
+        disableButton.innerHTML = "Enable";
+    }
+}
+
+
+disableButton.addEventListener("click", disableButtonAction);
+document.addEventListener("DOMContentLoaded", syncDisableSettings);
+document.addEventListener("DOMContentLoaded", syncClockwiseSettings);
 document.querySelector("form").addEventListener("submit", saveOptions);
